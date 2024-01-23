42bb9bb9c411d0c147118b017945d1068cb7caf1
MarianSofia31 <sofi@jausme.com>
2024-01-23 16:31:51 -0500

    estructura carpetas de login y register

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Register } from './views/Register/Register.view'
+import { Login } from './views/Login/Login.view'
 
 function App() {
 
   return (
-    <>
-      aaaaaaaaaaaaaaa
-    </>
+    <div>
+      <Routes>
+        <Route path='/register' element={Register()}/>
+        <Route path='/login' element={Login()}/>
+      </Routes>
+      
+    </div>
   )
 }
 
---
+++ src/views/Register/Register.view.tsx
@@ -0,0 +1,9 @@
+import style from './Register.module.css'
+
+export const Register = () => {
+  return (
+    <div className={style.bg}>
+      a
+    </div>
+  )
+}
