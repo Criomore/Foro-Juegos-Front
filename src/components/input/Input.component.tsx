df1a6f8f5bf0238d8e8d6a09b9f30d224d2e457f
jaider <jaiderlolxd@gail.com>
2024-01-23 16:55:23 -0500

    carpeta components

---
+++ src/components/input/Input.component.tsx
@@ -0,0 +1,11 @@
+import style from './input.module.css'
+
+export const Input = () => {
+  return (
+    <div className={style.container}>
+      <label htmlFor=''>
+        <input type='text' name='' id='' />
+      </label>
+    </div>
+  )
+}
--- src/views/Login/Login.view.tsx
+++ src/views/Login/Login.view.tsx
@@ -1,5 +1,13 @@
 import style from './Login.module.css'
 
 export const Login = () => {
-  return <div className={style.bg}>Login</div>
+  return (
+    <div className={style.bg}>
+      <div className={style.bgBlack}>
+        <div className={style.container}>
+          <h2>INICIA SESIÓN</h2>
+        </div>
+      </div>
+    </div>
+  )
 }
