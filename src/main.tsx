970a7d1de1d8392adb189ed435dcfa0d0f0f601f
jaider <jaiderlolxd@gail.com>
2024-05-20 17:40:22 -0500

    rb

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter } from 'react-router-dom'
+import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
 )
