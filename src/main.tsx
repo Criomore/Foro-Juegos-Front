3679a37ccda4cf9c3414e28c7daadecd678421ca
Marian-Sofia <sofiagutierrez0031@gmail.com>
2024-05-27 19:06:32 -0500

    profile

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import ReactDOM from 'react-dom/client'
--- src/views/Profile/Profile.view.tsx
+++ src/views/Profile/Profile.view.tsx
@@ -1,3 +1,4 @@
+import { Image } from 'react-bootstrap'
 import style from './Profile.module.css'
 
 export const Profile = () => {
@@ -7,15 +8,8 @@ export const Profile = () => {
         Aqui podras seguir configurando tu perfil. ¡Buena suerte!
       </div>
       <div className={style.container_portada}>
-        <div
-          style={{
-            backgroundImage:
-              'https://images.unsplash.com/photo-1707343843982-f8275f3994c5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8.png'
-          }}
-          className={style.portada_img}
-        ></div>
+        <Image src='src/assets/portada1.jpg' className={style.portada} />
       </div>
     </div>
   )
-}
-;<div></div>
+};
