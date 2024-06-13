0e82804242192d9913b107dcc37dd382e5da75cb
marian-sofia <sofiagutierrez0031@gmail.com>
2024-06-13 11:07:39 -0500

    profile

--- src/views/Profile/Profile.view.tsx
+++ src/views/Profile/Profile.view.tsx
@@ -4,12 +4,24 @@ import style from './Profile.module.css'
 export const Profile = () => {
   return (
     <div className={style.profile_container}>
-      <div className={style.container_goodluck}>
-        Aqui podras seguir configurando tu perfil. ¡Buena suerte!
-      </div>
       <div className={style.container_portada}>
         <Image src='src/assets/portada1.jpg' className={style.portada} />
+        <div className={style.portada_info}>
+          <div className={style.contain_names}>
+            <div className={style.apodo}>NutriaAguada</div>
+            <div className={style.name}>Marian Sofia</div>
+          </div>
+          <div className={style.contain_names}>
+            <div className={style.nac}>NutriaAguada</div>
+            <div className={style.cum}>Marian Sofia</div>
+          </div>
+          <Image/>
+          <div className={style.contain_names}>
+            <div className={style.nac}>NutriaAguada</div>
+            <div className={style.cum}>Marian Sofia</div>
+          </div>
+        </div>
       </div>
     </div>
   )
-};
+}
